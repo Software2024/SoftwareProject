5269958c60849d71de3c95bb555d11a227f33433
zaina <zaina@LAPTOP-CO14LE9B>
2024-04-03 00:00:14 +0300

    fixed

--- src/main/java/softwareproject/Main.java
+++ src/main/java/softwareproject/Main.java
@@ -2,12 +2,13 @@ package softwareproject;
 
 
 
-import interface1.WelcomePage;
+import interfacce.WelcomePage;
 
 public class Main {
 
 	   public static void main(String[] args) {
 		
 	      new WelcomePage().setVisible(true);
+	      
 	  }
 }
